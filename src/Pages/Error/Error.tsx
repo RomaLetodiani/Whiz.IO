8f2efc18f217ed7a4286337ecd7c33643633bef0
RomaLetodiani <roman.letodiani03@gmail.com>
2024-06-26 18:01:39 +0400

    Error Page

--- src/Pages/Error/Error.tsx
+++ src/Pages/Error/Error.tsx
@@ -1,14 +1,11 @@
-import { useRouteError } from "react-router-dom"
+import { ErrorPageStyles } from "./Error.styles"
 
 export default function ErrorPage() {
-  const error = useRouteError()
-  console.error(error)
-
   return (
-    <div id="error-page">
+    <ErrorPageStyles>
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>{/* <i>{error.statusText || error.message}</i> */}</p>
-    </div>
+    </ErrorPageStyles>
   )
 }
