7f2d3f05048e25dadefbc57587f5fd1c3ef90ed9
RomaLetodiani <roman.letodiani03@gmail.com>
2024-06-27 18:08:25 +0400

    Additional Development Section

---
+++ src/Pages/Products/Chat/Views/AdditionalDevelopment/AdditionalDevelopment.styled.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components"
+
+export const AdditionalDevelopmentStyles = styled.div`
+  h2 {
+    text-align: center;
+  }
+
+  > div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    @media (max-width: 768px) {
+      flex-direction: column;
+      padding-block: 0px;
+    }
+
+    > div {
+      flex: 1;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+      p {
+        max-width: 400px;
+        margin-inline: auto;
+      }
+    }
+  }
+`
--- src/Pages/Products/Chat/Views/AdditionalDevelopment/AdditionalDevelopment.tsx
+++ src/Pages/Products/Chat/Views/AdditionalDevelopment/AdditionalDevelopment.tsx
@@ -1,5 +1,29 @@
+import { additionalDevelopment } from "../../../../../Assets/Images/Chat"
+import SectionWrapper from "../../../../../Components/SectionWrapper/SectionWrapper"
+import { AdditionalDevelopmentStyles } from "./AdditionalDevelopment.styled"
+
 const AdditionalDevelopment = () => {
-  return <div>AdditionalDevelopment</div>
+  return (
+    <SectionWrapper>
+      <AdditionalDevelopmentStyles>
+        <h2>Additional Development</h2>
+        <div>
+          <div>
+            <img
+              src={additionalDevelopment}
+              alt="Additional Development Image"
+            />
+          </div>
+          <div>
+            <p>
+              თუ აღმოაჩენთ, რომ ჩვენს ჩათს არ აქვს თქვენი პერსონალური საჭიროების
+              რაიმე მოდული, დაგვიკვეთეთ და მიიღებთ ოპტიმალურ ვადებში.
+            </p>
+          </div>
+        </div>
+      </AdditionalDevelopmentStyles>
+    </SectionWrapper>
+  )
 }
 
 export default AdditionalDevelopment
