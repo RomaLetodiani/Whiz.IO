0f2dde2f9998465602d7ff2d5b1c31134f2d4da1
RomaLetodiani <roman.letodiani03@gmail.com>
2024-06-27 11:15:55 +0400

    Chat Page Images

---
+++ src/Assets/Images/Chat/index.ts
@@ -0,0 +1,63 @@
+// Hero
+import branding from "./Branding.png"
+
+// Integrate
+import integration from "./integrateChat.png"
+
+// TODO: add images for the following component
+// Configuration
+
+// Bot
+import bot from "./bot.png"
+
+// UserIdentification
+import userIdentification from "./userIdentification.png"
+
+// CommunicationManagement
+import communicationManagement1 from "./CommunicationManagement/1.png"
+import communicationManagement2 from "./CommunicationManagement/2.png"
+import communicationManagement3 from "./CommunicationManagement/3.png"
+
+// AppealsDetails
+// TODO: main photo here
+import appealsDetails1 from "./AppealsDetails/1.svg"
+import appealsDetails2 from "./AppealsDetails/2.svg"
+import appealsDetails3 from "./AppealsDetails/3.svg"
+import appealsDetails4 from "./AppealsDetails/4.svg"
+import appealsDetails5 from "./AppealsDetails/5.svg"
+import appealsDetails6 from "./AppealsDetails/6.svg"
+import appealsDetails7 from "./AppealsDetails/7.svg"
+import appealsDetails8 from "./AppealsDetails/8.svg"
+import appealsDetails9 from "./AppealsDetails/9.svg"
+import appealsDetails10 from "./AppealsDetails/10.svg"
+
+// LiveMonitoring
+// TODO: main photo here
+// TODO: line svg here
+
+// APIintegration
+import apiIntegration from "./ApiIntegration.png"
+
+// AdditionalDevelopment
+import additionalDevelopment from "./additionalDevelopment.png"
+export {
+  branding,
+  integration,
+  bot,
+  userIdentification,
+  communicationManagement1,
+  communicationManagement2,
+  communicationManagement3,
+  appealsDetails1,
+  appealsDetails2,
+  appealsDetails3,
+  appealsDetails4,
+  appealsDetails5,
+  appealsDetails6,
+  appealsDetails7,
+  appealsDetails8,
+  appealsDetails9,
+  appealsDetails10,
+  apiIntegration,
+  additionalDevelopment,
+}
