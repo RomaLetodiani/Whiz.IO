4be6e6ce6bda4d5477866ce5d18097bfcc73c86f
RomaLetodiani <roman.letodiani03@gmail.com>
2024-06-28 12:23:32 +0400

    Appeals Details Section

--- src/Pages/Products/Chat/Views/AppealsDetails/AppealsDetails.tsx
+++ src/Pages/Products/Chat/Views/AppealsDetails/AppealsDetails.tsx
@@ -1,5 +1,32 @@
+import { mainAppealsDetails } from "../../../../../Assets/Images/Chat"
+import SectionWrapper from "../../../../../Components/SectionWrapper/SectionWrapper"
+import { AppealsDetailsStyles } from "./AppealsDetails.styled"
+import { AppealsDetailsImages } from "./Utils/AppealsDetailsImages"
+
 const AppealsDetails = () => {
-  return <div>AppealsDetails</div>
+  return (
+    <SectionWrapper>
+      <AppealsDetailsStyles>
+        <div className="AppealsDetailsHeader">
+          <h2>მომართვის დეტალები</h2>
+          <p>
+            მომართვასთან დაკავშირებული ნებისმიერი ტიპის ტექნიკური ინფორმაცია თუ
+            KPI მაჩვენებელი მარტივად ხელმისაწვდომია თქვენთვის
+          </p>
+        </div>
+        <div className="AppealsDetailsImages">
+          {AppealsDetailsImages.map((item, index) => (
+            <div key={index}>
+              <img src={item.img} alt={item.alt} />
+            </div>
+          ))}
+        </div>
+        <div className="AppealsDetailsImageDiv">
+          <img src={mainAppealsDetails} alt="Appeals Details" />
+        </div>
+      </AppealsDetailsStyles>
+    </SectionWrapper>
+  )
 }
 
 export default AppealsDetails
---
+++ src/Pages/Products/Chat/Views/AppealsDetails/Utils/AppealsDetailsImages.ts
@@ -0,0 +1,55 @@
+import {
+  appealsDetails1,
+  appealsDetails10,
+  appealsDetails2,
+  appealsDetails3,
+  appealsDetails4,
+  appealsDetails5,
+  appealsDetails6,
+  appealsDetails7,
+  appealsDetails8,
+  appealsDetails9,
+} from "../../../../../../Assets/Images/Chat"
+
+export const AppealsDetailsImages = [
+  {
+    img: appealsDetails1,
+    alt: "მომართვის ID",
+  },
+  {
+    img: appealsDetails2,
+    alt: "თარიღი და დრო",
+  },
+  {
+    img: appealsDetails3,
+    alt: "წყარო",
+  },
+  {
+    img: appealsDetails4,
+    alt: "ნაპასუხები/უპასუხო",
+  },
+  {
+    img: appealsDetails5,
+    alt: "მომართვის მიმღები",
+  },
+  {
+    img: appealsDetails6,
+    alt: "ლოდინის ხანგრძლივობა",
+  },
+  {
+    img: appealsDetails7,
+    alt: "მიმოწერის ხანგრძლივობა",
+  },
+  {
+    img: appealsDetails8,
+    alt: "დამუშავების ხანგრძლივობა",
+  },
+  {
+    img: appealsDetails9,
+    alt: "შეყოვნების ხანგრძლივობა",
+  },
+  {
+    img: appealsDetails10,
+    alt: "რედაქტირების ხანგრძლივობა",
+  },
+]
