5bc0e7c19cd9268778d741bd565e5bc14d164924
RomaLetodiani <roman.letodiani03@gmail.com>
2024-07-05 21:35:30 +0400

    Menu Modules Icons

---
+++ src/Components/Shared/MenuIcons.ts
@@ -0,0 +1,33 @@
+import livePanel from "../../Assets/Icons/Menu/Modules/livepanel.png"
+import requests from "../../Assets/Icons/Menu/Modules/Requests.png"
+import tasks from "../../Assets/Icons/Menu/Modules/tasks.png"
+import cases from "../../Assets/Icons/Menu/Modules/cases.png"
+import campaign from "../../Assets/Icons/Menu/Modules/campaigns.png"
+import CRM from "../../Assets/Icons/Menu/Modules/CRM.png"
+import monitoring from "../../Assets/Icons/Menu/Modules/monitoring.png"
+import internalCollaboration from "../../Assets/Icons/Menu/Modules/internalCollaboration.png"
+import WFM from "../../Assets/Icons/Menu/Modules/WFM.png"
+import ticketing from "../../Assets/Icons/Menu/Modules/Ticketing.png"
+import knowledgeBase from "../../Assets/Icons/Menu/Modules/knowledgeBase.png"
+import logs from "../../Assets/Icons/Menu/Modules/logs.png"
+import autoDialer from "../../Assets/Icons/Menu/Modules/autoDialer.png"
+import autoInformer from "../../Assets/Icons/Menu/Modules/autoInformer.png"
+import API from "../../Assets/Icons/Menu/Modules/API.png"
+
+export {
+  livePanel,
+  requests,
+  tasks,
+  cases,
+  campaign,
+  CRM,
+  monitoring,
+  internalCollaboration,
+  WFM,
+  ticketing,
+  knowledgeBase,
+  logs,
+  autoDialer,
+  autoInformer,
+  API,
+}
