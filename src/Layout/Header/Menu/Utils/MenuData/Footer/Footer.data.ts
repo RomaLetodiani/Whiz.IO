2e918d020ad24c76075b8bf9e74a7642332a550d
RomaLetodiani <roman.letodiani03@gmail.com>
2024-07-06 16:33:36 +0400

    Menu Footer Data

---
+++ src/Layout/Header/Menu/Utils/MenuData/Footer/Footer.data.ts
@@ -0,0 +1,18 @@
+export const menuDataFooter = [
+  {
+    icon: "icon",
+    title: "Changelog",
+  },
+  {
+    icon: "icon",
+    title: "საფორთი",
+  },
+  {
+    icon: "icon",
+    title: "Full API reference",
+  },
+  {
+    icon: "API სტატუსი",
+    title: "სახელი",
+  },
+]
