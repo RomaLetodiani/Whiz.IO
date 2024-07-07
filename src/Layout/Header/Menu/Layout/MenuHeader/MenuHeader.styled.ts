9600b7c464539ec92605d47da4f7c748d4855f90
RomaLetodiani <roman.letodiani03@gmail.com>
2024-07-07 12:24:50 +0400

    Menu Header

---
+++ src/Layout/Header/Menu/Layout/MenuHeader/MenuHeader.styled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+export const MenuHeaderStyles = styled.div`
+  padding-bottom: 0px !important;
+
+  h3 {
+    line-height: 24px;
+    margin-bottom: 4px;
+  }
+
+  p {
+    font-size: 14px;
+    line-height: 20px;
+  }
+`
---
+++ src/Layout/Header/Menu/Layout/MenuHeader/MenuHeader.tsx
@@ -0,0 +1,12 @@
+import { MenuHeaderStyles } from "./MenuHeader.styled"
+
+const MenuHeader = () => {
+  return (
+    <MenuHeaderStyles>
+      <h3>ყველაფერი იწყება კონტაქტით</h3>
+      <p>Get Started and learn more about our products.</p>
+    </MenuHeaderStyles>
+  )
+}
+
+export default MenuHeader
