94d6022de9accb3801c88c07fc945578e7b66954
RomaLetodiani <roman.letodiani03@gmail.com>
2024-07-07 15:39:32 +0400

    Added Soon

--- src/Layout/Header/Menu/Utils/MenuData/Products/CommunicationChannels.data.ts
+++ src/Layout/Header/Menu/Utils/MenuData/Products/CommunicationChannels.data.ts
@@ -1,3 +1,10 @@
+import { Logo } from "../../../../../../Components/Shared/icons"
 import { menuDataItemI } from "../../Menu.types"
 
-export const communicationChannels: menuDataItemI[] = []
+export const communicationChannels: menuDataItemI[] = [
+  {
+    icon: Logo,
+    title: "Added Soon",
+    desc: "We are working on this section. Please check back later.",
+  },
+]
